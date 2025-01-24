20d09387d6973a4c153de7d998ed6431e9bf220d
Chris Jeong <chrisjeong@Chriss-MacBook-Pro-22944.local>
2025-01-24 10:36:28 -0500

    First

---
+++ src/crypto/keygen.rs
@@ -0,0 +1,10 @@
+// This file handles the generation of the secret key and public key
+
+// use curve25519-dalek::
+pub(crate) fn secret_keygen() -> Result<u64, String> {
+    Ok(1)
+}
+
+// pub(crate) fn public_keygen() -> Result<String, String> {
+    
+// }
